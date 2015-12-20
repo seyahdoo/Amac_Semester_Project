2b72f62f180d7079fec71a468c7f196434046add
Seyyid Ahmed Doğan <seyyidahmed.dogann@gmail.com>
2015-12-20 13:38:05 +0200

    added Memory Control for strings, addded some file read code

--- Project_c/main.c
+++ Project_c/main.c
@@ -45,7 +45,10 @@ int AssistantCount;
 
 int main()
 {
+    printf("enter to read asistants");
     ReadAssistantsFromFile();
+    printf("exit from read asistants\n");
+
     ReadAssistantWorks();
 
 
@@ -63,10 +66,11 @@ int GotoNextLine(FILE *fp)
     do
     {
         c = getc(fp);
-    }while (c==10 && c==EOF);
+    }while (!(c==10 || c==EOF));
 
     return c;
     //fseek()
+    //ftell
 }
 
 void ReadAssistantWorks()
@@ -80,22 +84,23 @@ void ReadAssistantWorks()
     //for every Assistant
     for(i=0;i<AssistantCount;i++)
     {
-        //Todo typo!
-        printf("\nGetting %s's work scedule", Assistants[i].Name);
+        printf("\nGetting %s's work schedule", Assistants[i].Name);
 
+        printf("%s",Assistants[i].Filename);
         //open file for that assistant
         fp = fopen(Assistants[i].Filename,"r");
         if(fp == NULL)
         {
-            //Todo typo!
-            printf("File open error\n- Assistant Scedule file");
-            system("PAUSE");
+            printf("File open error\n- Assistant %s's schedule file named %s",Assistants[i].Name,Assistants[i].Filename);
+            getch();
+            //system("PAUSE"); system pause is a very very bad idea.
             exit(10);//'0xA'
         }
         //printf("File opened");
 
         c = GotoNextLine(fp);
         c = GotoNextLine(fp);
+
 
         char Start_hour = 0;
         char End_Hour = 0;
@@ -104,13 +109,53 @@ void ReadAssistantWorks()
 
         //Start hour first digit
         c = getc(fp);
-        Start_hour += (10*c);
+        Start_hour += (10*(c-'0'));
 
         //Start hour second digit
         c = getc(fp);
-        Start_hour += c;
+        Start_hour += (c-'0');
 
+        //printf("%d",Start_hour);
 
+        c = getc(fp); // [Dot]
+
+        //Start Minute First digit
+        c = getc(fp);
+        Start_Minute += (10*(c-'0'));
+
+        //Start Minute second digit
+        c = getc(fp);
+        Start_Minute += c-'0';
+
+        //printf("%d",Start_Minute);
+
+        c = getc(fp); // [Space]
+        c = getc(fp); // [-]
+        c = getc(fp); // [Space]
+
+        //End Hour First digit
+        c = getc(fp);
+        End_Hour += (10*(c-'0'));
+
+        //End Hour second digit
+        c = getc(fp);
+        End_Hour += c-'0';
+
+        //printf("%d",End_Hour);
+
+        c = getc(fp); //[Dot]
+
+        //End Minute First digit
+        c = getc(fp);
+        End_Minute += (10*(c-'0'));
+
+        //End Minute second digit
+        c = getc(fp);
+        End_Minute += c-'0';
+
+        //printf("%d",End_Minute);
+
+        c = getc(fp); //[;]
 
 
 
@@ -121,7 +166,8 @@ void ReadAssistantWorks()
     }
 
 
-
+    printf("\n finished reading asssistant works from file");
+    exit(1);
 
 }
 
@@ -148,7 +194,14 @@ int ReadAssistantLine(ASSISTANT *a,FILE *fp)
         //Calculate Length
         Length++;
         //Allocate for string
-        a->ID = realloc(a->ID,(Length+1)*sizeof(char));
+        char *temp = realloc(a->ID,(Length+1)*sizeof(char));
+        if (temp == NULL)
+        {
+            printf("Memory error.");
+        }
+        a->ID = temp;
+
+
         //Get the last Character
         a->ID[Length-1] = c;
     }
@@ -157,7 +210,7 @@ int ReadAssistantLine(ASSISTANT *a,FILE *fp)
     a->ID[Length] = NULL;
 
     //print as we read
-	//printf("%s\n",a->ID);
+	printf("%s\n",a->ID);
 
 	//Get Name
     a->Name = malloc(0*sizeof(char));
@@ -167,7 +220,13 @@ int ReadAssistantLine(ASSISTANT *a,FILE *fp)
         //Calculate Length
         Length++;
 		//Allocate for string
-        a->Name = realloc(a->Name,Length*sizeof(char));
+        char *temp = realloc(a->Name,(Length+1)*sizeof(char));
+        if (temp == NULL)
+        {
+            printf("Memory error.");
+        }
+        a->Name = temp;
+
         //Get the last Character
         a->Name[Length-1] = c;
     }
@@ -176,7 +235,7 @@ int ReadAssistantLine(ASSISTANT *a,FILE *fp)
     a->Name[Length] = NULL;
 
     //print as we read
-	//printf("%s\n",a->Name);
+	printf("%s\n",a->Name);
 
 	//Get Surname
     a->Surname = malloc(0*sizeof(char));
@@ -186,7 +245,13 @@ int ReadAssistantLine(ASSISTANT *a,FILE *fp)
         //Calculate Length
         Length++;
 		//Allocate for string
-        a->Surname = realloc(a->Surname,Length*sizeof(char));
+        char *temp = realloc(a->Surname,(Length+1)*sizeof(char));
+        if (temp == NULL)
+        {
+            printf("Memory error.");
+        }
+        a->Surname = temp;
+
         //Get the last Character
         a->Surname[Length-1] = c;
     }
@@ -195,7 +260,7 @@ int ReadAssistantLine(ASSISTANT *a,FILE *fp)
     a->Surname[Length] = NULL;
 
     //print as we read
-	//printf("%s",a->Surname);
+	printf("%s",a->Surname);
 
 	//Get Surname
     a->Filename = malloc(0*sizeof(char));
@@ -207,22 +272,34 @@ int ReadAssistantLine(ASSISTANT *a,FILE *fp)
         //Calculate Length
         Length++;
 		//Allocate for string
-        a->Filename = realloc(a->Filename,Length*sizeof(char));
+        char *temp = realloc(a->Filename,(Length+1)*sizeof(char));
+        if (temp == NULL)
+        {
+            printf("Memory error.");
+        }
+        a->Filename = temp;
+
         //Get the last Character
         a->Filename[Length-1] = c;
 		//get a char from file
 		c = getc(fp);
     }
 
-    //End the string
-    a->Filename[Length] = NULL;
-
     //print as we read
 	//printf("%s",a->Filename);
 
 	//append .csv to filename.
 	Length+=4;
-	a->Filename = realloc(a->Filename,Length*sizeof(char));
+
+	//allocate for it
+	char *temp = realloc(a->Filename,(Length+1)*sizeof(char));
+    if (temp == NULL)
+    {
+        printf("Memory error.");
+    }
+    a->Filename = temp;
+
+    //finally, write
 	a->Filename[Length-4]='.';
 	a->Filename[Length-3]='c';
 	a->Filename[Length-2]='s';
@@ -257,7 +334,7 @@ void ReadAssistantsFromFile()
         system("PAUSE");
         exit(10);//'0xA'
     }
-    //printf("File opened");
+    printf("File opened");
 
 	//define a place for assistants
 	Assistants = malloc(0);
@@ -270,13 +347,17 @@ void ReadAssistantsFromFile()
     {
         //Keep Count of Assistants
         AssistantCount++;
+        //printf("AssistantCount++");
 
         //Allocate for assistants
         Assistants = realloc(Assistants,AssistantCount*sizeof(ASSISTANT));
+        //printf("Assistants = realloc(Assistants,AssistantCount*sizeof(ASSISTANT));");
 
         //Read one assistant
         c = ReadAssistantLine(&Assistants[AssistantCount-1],fp);
+        //printf("c = ReadAssistantLine(&Assistants[AssistantCount-1],fp);");
 
+        //printf("\n%d",c);
         //PrintAssistant(&Assistants[AssistantCount-1]);
     }while (c != EOF);//if is the end of file
 
